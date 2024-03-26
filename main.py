519b0343ad5423198a225e0aae9df13fdd6bafae
joblator <janjoel.bonisch@gmail.com>
2024-03-26 09:28:31 +0200

    first main loop

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print(123)
+
+import pygame
+import time
+
+pygame.init()
+SPACESHIP_SPEED = 100
+screen = pygame.display.set_mode([500, 500])
+pygame.display.set_caption('galaga in pygame')
+spaceship_y = 20
+spaceship_X = 20
+
+keys = pygame.key.get_pressed()
+    
+if keys[pygame.K_LEFT] and >0: 
+    x -= speed
+if keys[pygame.K_RIGHT] and <500: 
+    x += speed 
+if keys[pygame.K_UP] and >0: 
+    y -= speed    
+if keys[pygame.K_DOWN] and <500:   
+    y += speed 
+
+pygame.quit()
